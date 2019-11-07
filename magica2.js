62e415aada59d8f9e9c28422878e2fa51a7a29cf
Ícaro Pinto do Nascimento <38851@pjmt.local>
2019-11-07 17:21:43 -0400

    Alterações para deixar apenas um botão na tela

--- magica2.js
+++ magica2.js
@@ -1,42 +1,13 @@
-document.getElementById("ajustaCpf").addEventListener("click", ajustaCpf);
-document.getElementById("ajustaCnpj").addEventListener("click", ajustaCnpj);
-document.getElementById("ajustaProcesso").addEventListener("click", ajustaProcesso);
+document.getElementById("removePontos").addEventListener("click", removePontos);
 
-function ajustaCpf(){
-  var cpf = prompt("Cole aqui o CPF: ");
-  cpf.trim();
-  var cpfNormal = new RegExp(/\D/g);
-  var txt = cpf.replace(cpfNormal,'');
-  
-  if(txt.length == 11) {
-  	alert("CPF: " + txt);
-  } else {
-  	alert("CPF inválido!");
-  }
-}
 
-function ajustaCnpj(){
-  var cpf = prompt("Cole aqui o CNPJ: ");
-  cpf.trim();
-  var cpfNormal = new RegExp(/\D/g);
-  var txt = cpf.replace(cpfNormal,'');
+function removePontos(){
+  var texto = prompt("Cole aqui o CPF/CNPJ ou Nº do Processo: ");
+  texto.trim();
+  var textoAlterado = new RegExp(/\D/g);
+  var txt = texto.replace(textoAlterado,'');
   
-  if(txt.length == 14) {
-  	alert("CNPJ: <strong>" + txt + "</strong>");
-  } else {
-  	alert("CNPJ inválido!");
-  }
-}
-
-function ajustaProcesso(){
-  var cpf = prompt("Cole aqui o nº do Processo: ");
-  cpf.trim();
-  var cpfNormal = new RegExp(/\D/g);
-  var txt = cpf.replace(cpfNormal,'');
+  alert(txt);
   
-  if(txt.length == 20) {
-  	alert("Nº do Processo: " + txt);
-  } else {
-  	alert("Nº do Processo inválido!");
-  }
+  
 }
